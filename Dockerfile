3d3690e6e65e9309b4b617c4a0e2189cf1e9a2b7
Syatimwaraph <syatimwaraph@gmail.com>
2026-06-04 13:08:49 +0200

    THIRD COMMIT AND FIRST VERSION OF JOES QUENCALLERIE EDITED DOCKER FILE FOR DEPLOYEMENT PREPARATION

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,28 @@
-FROM ubuntu:latest
-LABEL authors="raph"
+FROM eclipse-temurin:17-jdk
 
-ENTRYPOINT ["top", "-b"]
+WORKDIR /app
+
+COPY target/*.jar app.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java","-jar","app.jar"]
+
+
+
+## Use Java 17
+#FROM eclipse-temurin:21-jdk
+#
+#LABEL authors="raph"
+#
+## Working directory
+#WORKDIR /app
+#
+## Copy jar
+#COPY target/*.jar app.jar
+#
+## Expose Spring Boot port
+#EXPOSE 8080
+#
+## Start application
+#ENTRYPOINT ["java","-jar","app.jar"]
--- src/main/java/dev/syatimwaraph/quencallerie_mngt_v1/QuencallerieMngtV1Application.java
+++ src/main/java/dev/syatimwaraph/quencallerie_mngt_v1/QuencallerieMngtV1Application.java
@@ -17,43 +17,43 @@ public class QuencallerieMngtV1Application {
         SpringApplication.run(QuencallerieMngtV1Application.class, args);
     }
 
-    @SpringBootApplication
-    public class QuencallerieApplication {
-
-        public static void main(String[] args) {
-            SpringApplication.run(QuencallerieApplication.class, args);
-        }
-
-        /**
-         * Creates a default ADMIN user when the application starts.
-         * The user is created only if no user with the username "admin" exists.
-         */
-//        @Bean
-//        CommandLineRunner createDefaultAdmin(UserOrgRepository userRepository,
-//                                             PasswordEncoder passwordEncoder) {
-//            return args -> {
+//    @SpringBootApplication
+//    public class QuencallerieApplication {
 //
-//                String username = "saler";
-//                String rawPassword = "saler123";
-//
-//                if (userRepository.findByUsername(username).isEmpty()) {
-//
-//                    UserOrg admin = new UserOrg();
-//                    admin.setUsername(username);
-//                    admin.setPassword(passwordEncoder.encode(rawPassword));
-//                    admin.setRole(Roles.SALER);
-//
-//                    userRepository.save(admin);
-//
-//                    System.out.println("======================================");
-//                    System.out.println("Default SALER user created");
-//                    System.out.println("Username: saler");
-//                    System.out.println("Password: saler123");
-//                    System.out.println("======================================");
-//                } else {
-//                    System.out.println("SALER user already exists.");
-//                }
-//            };
+//        public static void main(String[] args) {
+//            SpringApplication.run(QuencallerieApplication.class, args);
 //        }
-    }
+//
+//        /**
+//         * Creates a default ADMIN user when the application starts.
+//         * The user is created only if no user with the username "admin" exists.
+//         */
+////        @Bean
+////        CommandLineRunner createDefaultAdmin(UserOrgRepository userRepository,
+////                                             PasswordEncoder passwordEncoder) {
+////            return args -> {
+////
+////                String username = "saler";
+////                String rawPassword = "saler123";
+////
+////                if (userRepository.findByUsername(username).isEmpty()) {
+////
+////                    UserOrg admin = new UserOrg();
+////                    admin.setUsername(username);
+////                    admin.setPassword(passwordEncoder.encode(rawPassword));
+////                    admin.setRole(Roles.SALER);
+////
+////                    userRepository.save(admin);
+////
+////                    System.out.println("======================================");
+////                    System.out.println("Default SALER user created");
+////                    System.out.println("Username: saler");
+////                    System.out.println("Password: saler123");
+////                    System.out.println("======================================");
+////                } else {
+////                    System.out.println("SALER user already exists.");
+////                }
+////            };
+////        }
+//    }
 }
